23445dc9e332e2cbb660849ccc7fa038bc7f2a0b
TheGabrielGalvao <gabriel.galvao736@gmail.com>
2022-11-09 21:07:08 -0300

    Teste Everton

--- src/components/molecules/SideMenu.tsx
+++ src/components/molecules/SideMenu.tsx
@@ -1,5 +1,5 @@
 import clsx from "clsx"
-import { AddressBook, CaretRight, CurrencyDollarSimple, Gear, HouseSimple, TiktokLogo } from "phosphor-react"
+import { AddressBook, CaretRight, CurrencyDollarSimple, Envelope, FileSearch, Gear, HouseSimple, TiktokLogo } from "phosphor-react"
 import { ReactNode, useState } from "react"
 import { EPositionItemMenu } from "../../interfaces/ui/navigation"
 import { simpleNavigation } from "../../util/constants/navigation"
@@ -7,6 +7,7 @@ import { getMenuByPosition } from "../../util/helpers/ui"
 import { Logo } from "../atoms/Logo"
 import { Sidebar } from "../atoms/SideBar"
 import { Text } from "../atoms/Text"
+import { TextInput } from "../atoms/TextInput"
 
 export interface SideMenuProps {
 }
@@ -26,7 +27,7 @@ export const SideMenu = () => {
       <Sidebar.Nav open={open}>
             <CaretRight 
               className={clsx(
-                'absolute cursor-pointer -right-3 top-9  bg-blue-500 text-white border-2 rounded-full',
+                'absolute cursor-pointer -right-3 top-9  bg-blue-100 text-white border-2 rounded-full',
                 {
                   'rotate-180': open
                 }
@@ -58,6 +59,12 @@ export const SideMenu = () => {
             </Sidebar.NavSection>
 
             <Sidebar.NavSection open={open}>
+              <TextInput.Root>
+                <TextInput.Icon>
+                  <Envelope className="cursor-pointer" onClick={() => setOpen(!open)}/>
+                </TextInput.Icon>
+                {/* <TextInput.Input placeholder= "Pesquisar" /> */}
+              </TextInput.Root>
                 {middleNav.map(
                     item => (
                       <Sidebar.NavSectionItem open={open} icon={item.icon} label={<Text>{item.label}</Text>} />
