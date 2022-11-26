e81225fcaf0b8bce7e9f120e28b15a9b5007f187
TheGabrielGalvao <gabriel.galvao736@gmail.com>
2022-11-25 23:44:26 -0300

    Ajuste de layout

--- src/components/molecules/Calendar.tsx
+++ src/components/molecules/Calendar.tsx
@@ -1,19 +1,12 @@
-import '@fullcalendar/react/dist/vdom'
-import FullCalendar from '@fullcalendar/react' // must go before plugins
-import timeGridPlugin from '@fullcalendar/timegrid' // a plugin!
 import { CalendarElement } from "../atoms"
 
 
 export const Calendar = () => {
   return(
-    <FullCalendar
-        plugins={[ timeGridPlugin ]}
-        initialView="timeGridDay"
-      />
-    // <CalendarElement.Root>
-    //   <CalendarElement.Controls />
-    //   <CalendarElement.Header />
-    //   <CalendarElement.Content />
-    // </CalendarElement.Root>
+    <CalendarElement.Root>
+      <CalendarElement.Controls />
+      <CalendarElement.Header />
+      <CalendarElement.Content />
+    </CalendarElement.Root>
   )
 }
